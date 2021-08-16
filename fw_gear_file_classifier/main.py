022c74480f53dbf94fa4f32bac3abcd54585e337
Nate Richman <naterichman@flywheel.io>
2021-08-16 14:08:18 -0500

    MAIN: Add classification-toolkit as submodule

--- fw_gear_file_classifier/main.py
+++ fw_gear_file_classifier/main.py
@@ -1,12 +1,20 @@
 """Main module."""
-
 from typing import Any, Dict
 import logging
+from pathlib import Path
+import json
+
+from fw_classification.adapters import FWAdapter
+from fw_classification.profiles import get_profile
 
 log = logging.getLogger(__name__)
-from fw_classification.adapters import FWObject
 
+def run(file_input: Dict[str, Any], out_dir: Path) -> int:
+    """Main run function."""
+    fw_adapter = FWAdapter(file_input)
+    metadata = fw_adapter.run(get_profile('main.yml'))
+    log.info('Writing metadata:\n {json.dumps(metadata, indent=2)}')
+    with open(out_dir / '.metadata.json', 'w') as fp:
+        json.dump(metadata, fp, indent=2)
 
-def run(file_input: Dict[str, Any]) -> int:
-    fw_adapter = FWObject(file_input)
-    fw_adapter.run(profile)
+    return 0
