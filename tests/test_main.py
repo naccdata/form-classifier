b9761df9fdd35c600014ecfc672af111801570b3
Nate Richman <naterichman@flywheel.io>
2021-09-09 10:40:23 -0500

    ENH: Add tests, fix linting issues, remove dev classification-toolkit

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,28 @@
 """Module to test main.py"""
+from unittest.mock import MagicMock
+
 import pytest
 
-# from fw_gear_file_classifier.main import run
+from fw_gear_file_classifier.main import classify
 
 
-def test_run():
-    """Here is an example on how the test looks like for run.py
-
-
-    exit_code = run()
-    assert exit_code == 0
-
-    """
-    pass
+@pytest.mark.parametrize(
+    "to_mock, file_type",
+    [
+        ("fw_gear_file_classifier.main.NiftiFWAdapter", "nifti"),
+        ("fw_gear_file_classifier.main.FWAdapter", "dicom"),
+    ],
+)
+def test_classify_nifti(mocker, to_mock, file_type):
+    adapt = mocker.patch(to_mock)
+    add_qc = mocker.patch("fw_gear_file_classifier.main.add_qc_info")
+    file_input = {"object": {"type": file_type}, "location": {"name": "test"}}
+    gear_context = MagicMock()
+    profile = MagicMock()
+    _ = classify(file_input, gear_context, profile)
+    adapt.assert_called_once_with(file_input, gear_context)
+    adapt.return_value.classify.assert_called_once_with(profile)
+    assert (gear_context, file_input) in add_qc.call_args
+    gear_context.update_file_metadata.assert_called_once_with(
+        "test", info=add_qc.return_value
+    )
