f00012b21c0e9f222c08cf1a34619b8bf0786ac3
Nate Richman <naterichman@flywheel.io>
2021-12-21 23:09:50 +0000

    MAIN: Fix classification profiles

--- bin/update_profiles.sh
+++ bin/update_profiles.sh
@@ -4,13 +4,15 @@ set -euo pipefail
 
 function update_submodules() {
     cd fw_gear_file_classifier/classification-profiles || exit
+    git submodule update --init
     git fetch
     git fetch --tags
     git checkout "$1"
 }
 
 update_submodules "$1"
-git add fw_gear_file_classifier/classification-profiles
+cd ../
+git add classification-profiles
 git commit --no-verify -m "MAIN: Update classification-profiles to version $1"
 
 
